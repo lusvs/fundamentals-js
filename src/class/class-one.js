d6b73f203dfa45979cd1955f141e0b7f695f1027
lusvs <lucasvssoares@gmail.com>
2024-09-30 21:49:10 -0300

    estudando js

--- src/class/class-one.js
+++ src/class/class-one.js
@@ -100,3 +100,70 @@ console.log(fullName.indexOf('de'), 'indexOf')
 // corta string por indice
 console.log(fullName.slice(8, 10))
 
+const remember = 'Mariazinha da Silva Souza'
+
+console.log(remember.length, 'remember')
+
+let arrayRemember = remember.split(' ')
+console.log(arrayRemember[0], 'split')
+console.log(remember.slice(0, 10), 'slice')
+
+//metodos de array 
+
+console.table(arrayRemember)
+console.log(arrayRemember.length, 'conta indice')
+
+const nameRule =  arrayRemember[0] + ' ' + arrayRemember[arrayRemember.length - 1]
+console.log(nameRule)
+
+// forma antiga
+arrayRemember[4] = 'isiri um item'
+console.table(arrayRemember)
+
+//usando metodo push
+arrayRemember.push('adicionei usando o metodo push')
+console.table(arrayRemember)
+
+// Visualizar o ultimo item
+console.log(arrayRemember[arrayRemember.length -1])
+
+// Remover o ultimo item
+arrayRemember.pop()
+console.log(arrayRemember)
+
+// Tira o primeiro item
+arrayRemember.shift()
+console.table(arrayRemember, 'shift')
+
+// Coloca um item em primeiro
+arrayRemember.unshift('novo item unshit')
+console.table(arrayRemember, 'unshift')
+
+// metodos de objetos
+const product = {
+    name: 'Camisa',
+    price: 15.98,
+    inStock: true,
+    brand: 'Oakley'
+}
+
+console.log(product)
+console.log(product.name)
+console.log(product.brand)
+
+const { inStock, brand, price } = product
+
+console.log(inStock, 'usando a desetruturação do js')
+
+//Tirei o estoque por desestruturação
+// const {inStock} = product
+// console.log(inStock)
+
+// adicionei um item direto do js
+product.color = ['Azul', 'Vermelho', 'Verde']
+
+
+// pegando o indice 1 do array dentro do objeto
+console.log(product.color[1])
+
+
