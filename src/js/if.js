5ed5f96dac8efa4315a5102793749f2fca38f572
lusvs <lucasvssoares@gmail.com>
2024-10-16 21:02:06 -0300

    revisao

---
+++ src/js/if.js
@@ -0,0 +1,13 @@
+const age = 17
+
+if (age >= 18){
+    return console.log('Você é maior')
+}
+
+else if (age === 17){
+    return console.log('Já já vc pode beber')    
+}
+
+else{
+   return console.log('Você é menor')
+}   
