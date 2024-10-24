d10d3f64f739d78308a99ed1bb21cf07626eeeea
lusvs <lucasvssoares@gmail.com>
2024-10-23 22:26:16 -0300

    feat: fzm o exercicio 3

---
+++ src/exercises/exercicio-3.js
@@ -0,0 +1,9 @@
+// Exercício 3: Somar os números de 1 a 100
+// Crie um loop que calcule a soma de
+
+//  todos os números de 1 a 100.
+let soma = 0
+for (i = 1; i <= 100; i++){
+    soma += i
+}
+console.log(soma)
---
+++ src/exercises/exercicio-4.js
@@ -0,0 +1,4 @@
+// Exercício 4: Imprimir os elementos de um array
+// Dado um array de frutas, use um loop para imprimir cada fruta.
+
+const  frutas = [{}] 
