6b8551b5211284e9e5b0b26dfbe077880e40d703
lusvs <lucasvssoares@gmail.com>
2024-11-18 20:53:40 -0300

    feat: exercicio-9

---
+++ src/exercises/exercicio-9.js
@@ -0,0 +1,7 @@
+// Exercício 9: Imprimir a tabuada de um número
+// Dado um número, use um loop para imprimir a tabuada desse número (de 1 a 10).
+
+let numero = 7; 
+for (let i = 1; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
