084dc77e0895b8eca15cb0916b4402910e6645d5
lusvs <lucasvssoares@gmail.com>
2024-11-18 20:45:30 -0300

    feat: exercicio-5

---
+++ src/exercises/exercicio-5.js
@@ -0,0 +1,6 @@
+// Exercício 5: Multiplicar os números de 1 a 10 por 2
+// Crie um loop que imprima o resultado da multiplicação de cada número de 1 a 10 por 2.
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i * 2);
+}
