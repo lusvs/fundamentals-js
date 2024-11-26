cf9af8beed0e0eb4fc85618e0ffe9a1fe3756e9d
lusvs <lucasvssoares@gmail.com>
2024-11-25 22:17:02 -0300

    feat: function.js

--- src/js/function.js
+++ src/js/function.js
@@ -19,17 +19,42 @@
 
 // console.log(Multiplicar(18, 10))
 
-function MediaQuery(){
-    const getWidthScreen = window.innerWidth
 
-    if(getWidthScreen < 400) {
-        console.log('é mobile')
-    } else if (getWidthScreen > 400 && getWidthScreen < 780) {
-        console.log('é tablet')
-    } else {
-        console.log('é desktop')
-    }
+// ========================================================
+// FAZER O SITE SABER O TAMANHO DA TELA AUTOMATICAMENTE
+// function MediaQuery(){
+//     const getWidthScreen = window.innerWidth
 
+//     if(getWidthScreen < 400) {
+//         console.log('é mobile')
+//     } else if (getWidthScreen > 400 && getWidthScreen < 780) {
+//         console.log('é tablet')
+//     } else {
+//         console.log('é desktop')
+//     }
+
+// }
+
+// MediaQuery()
+// ========================================================
+
+function ShortName(fullName) {
+
+    const splitName = fullName.split(' ')
+    // ['Lucas', 'Soares', 'Valerio', 'dos', 'Santos']
+
+    const [firstName, ...rest] = splitName
+    // [1, 2, 3, 4] = [1, ...rest] = [1, 2, 3]
+    const lastName = rest[rest.lenght -1]
+    console.log(lastName)
+    return console.log(`${firstName}`)
 }
+ShortName('Lucas Soares Valerio dos Santos')
 
-MediaQuery()
+const name = 'Diego Sarti Viana'
+const transform = name.split(' ')
+
+const [first, ...rest] = transform
+console.table(transform)
+console.log(first)
+console.log(rest)
