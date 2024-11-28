f27e885797f9c36d419d561f6da583c9c1569d6e
lusvs <lucasvssoares@gmail.com>
2024-11-27 21:06:51 -0300

    feat: fun.js .table

--- src/js/fun.js
+++ src/js/fun.js
@@ -1,3 +1,7 @@
 const fullName = 'Lucas Soares Valerio dos Santos'
 console.log(fullName.length, 'conta caracteres') //.length conta caracter (conta a partir do 1)
 
+const fruits = ['maça', 'pessego', 'uva', 'pera'] 
+console.log(fruits.length, 'array')
+console.table(fruits[fruits.length -1]) //.table deixa o array mais bonito
+
