0bfbcf11819e51fd5d3fe9e934cd25a3ca051526
lusvs <lucasvssoares@gmail.com>
2024-12-02 22:19:53 -0300

    feat api pokemon

--- src/js/apiPokemon.js
+++ src/js/apiPokemon.js
@@ -1,17 +1,18 @@
 
-function getPokemon() {
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+function getPokemon(pokemon) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
         .then(res => res.json())
         .then((data) => {
-            const {name,sprites, weight} = data;
-            const nome = `Nome: ${name}`;
-            const peso = `Peso: ${weight}`;
-            const linkFoto = `link da foto: ${sprites.front_default}`;
-            console.log(nome);
-            console.log(peso);
-            console.log(linkFoto);
+            const { name, sprites, weight } = data;
+            const div = document.getElementById('pokemon-info')
+            
+            div.innerHTML = `
+                <h2>${name}</h2>
+                <img src="${sprites.front_default}" alt=">
+                <p>${weight}</p>
+            `
         })
-        // .catch(err => console.error('O erro é:', err))
+        .catch((error) => console.error("O erro é:", error));
 }
-getPokemon()
+getPokemon('articuno')
 
